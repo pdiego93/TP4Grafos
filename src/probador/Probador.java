81c7470ee938afb285c49b2c92a928dbc27899b6
FlorKaucic <Frederick@Frederick-Win7>
2015-11-15 23:37:23 -0300

    Probador

--- src/probador/Probador.java
+++ src/probador/Probador.java
@@ -8,20 +8,31 @@ public class Probador {
 	public static void main(String[] args) {
 		GrafoNDNP in;
 		GrafoNDNP out;
-		
+
 		String rutaIn = "grafo.in";
 		String rutaOut = "coloreado.out";
-		
+
 		in = Archivo.leerGrafo(rutaIn);
 		out = Archivo.leerGrafoColoreado(rutaOut);
 
-		for(int i = 0; i < out.getCantNodos(); i++) {
-			int colori = out.getColor(i);
-			for(int j = i+1; j <  ; j++){
-				
-			}
-		}
-		
+		System.out.println(
+				test(in, out) ? 
+				"Se coloreo de manera correcta."
+				: "Error en el coloreo.");
+
 	}
 
+	private static boolean test(GrafoNDNP in, GrafoNDNP out) {
+		
+		for (int i = 0; i < out.getCantNodos(); i++) {
+			int colori = out.getColor(i);
+			if(colori == 0)
+				return false;
+			
+			for (int j = i + 1; j < out.getCantNodos(); j++)
+				if (in.getAdyacencia(i, j) && colori == out.getColor(j))
+					return false;
+		}
+		return true;
+	}
 }
