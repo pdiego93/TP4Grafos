b461f53a6a555c4f625e47cddd7d7d2503e5a8b5
FlorKaucic <Frederick@Frederick-Win7>
2015-11-16 00:12:54 -0300

    Mas casos probador

--- src/probador/Probador.java
+++ src/probador/Probador.java
@@ -15,21 +15,29 @@ public class Probador {
 		in = Archivo.leerGrafo(rutaIn);
 		out = Archivo.leerGrafoColoreado(rutaOut);
 
-		System.out.println(
-				test(in, out) ? 
-				"Se coloreo de manera correcta."
+		System.out.println(test(in, out) ? "Se coloreo de manera correcta."
 				: "Error en el coloreo.");
 
 	}
 
 	private static boolean test(GrafoNDNP in, GrafoNDNP out) {
-		
-		for (int i = 0; i < out.getCantNodos(); i++) {
+		int cc = out.getCantColores(), cn = out.getCantNodos();
+		if (cc < 1 || cc > cn)
+			return false;
+		if (cc > out.getCantAristas() + 1)
+			return false;
+		if (in.getCantNodos() != cn
+				&& in.getCantAristas() != out.getCantAristas()
+				&& in.getPorcAdy() != out.getPorcAdy()
+				&& in.getGradoMax() != out.getGradoMax()
+				&& in.getGradoMin() != out.getGradoMin())
+			return false;
+		for (int i = 0; i < cn; i++) {
 			int colori = out.getColor(i);
-			if(colori == 0)
+			if (colori == 0)
 				return false;
-			
-			for (int j = i + 1; j < out.getCantNodos(); j++)
+
+			for (int j = i + 1; j < cn; j++)
 				if (in.getAdyacencia(i, j) && colori == out.getColor(j))
 					return false;
 		}
